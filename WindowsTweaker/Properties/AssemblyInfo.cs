455a705ec2835264164d771f006c385b8fe938c7
Siddharth Srivastava <sidmkp96@gmail.com>
2013-05-26 13:19:23 +0530

    Created UIs for System, and partially for Display and Right-Click

--- WindowsTweaker/Properties/AssemblyInfo.cs
+++ WindowsTweaker/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SuRe Softwares")]
 [assembly: AssemblyProduct("WindowsTweaker")]
-[assembly: AssemblyCopyright("Copyright ©  2012, Siddharth Srivastava")]
+[assembly: AssemblyCopyright("Copyright ©  2013, Siddharth Srivastava")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
