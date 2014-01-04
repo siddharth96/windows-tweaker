a66672650f061c3c53b9ddd0d32972e6113a904b
Siddharth Srivastava <sidmkp96@gmail.com>
2014-01-05 02:33:20 +0530

    Created Custom CheckBox with Help Icon

---
+++ WindowsTweaker/HelpCheckBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WindowsTweaker
+{
+    /// <summary>
+    ///     Interaction logic for HelpCheckBox.xaml
+    /// </summary>
+    public partial class HelpCheckBox : UserControl
+    {
+        public HelpCheckBox()
+        {
+            InitializeComponent();
+        }
+
+        public String HelpText
+        {
+            get { return txtHelp.Text; }
+            set { txtHelp.Text = value; }
+        }
+
+        public String Text
+        {
+            get { return txtChkBx.Text; }
+            set { txtChkBx.Text = value; }
+        }
+
+        private void OnHelpBoxMouseEnter(object sender, MouseEventArgs e)
+        {
+            popHelp.IsOpen = true;
+        }
+    }
+}
