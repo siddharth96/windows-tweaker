b7b6f528ba1f5642ae252a0e9a631a2e6a33061d
Gaurav Manchanda <gmanchanda@manh.com>
2014-01-15 13:39:42 +0530

    test

--- WindowsTweaker/MainWindow.xaml.cs
+++ WindowsTweaker/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows.Media;
 
 namespace WindowsTweaker {
 
-    /// <summary>
+         /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
