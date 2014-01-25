bc921baf8e2d69ae0dd9f2c7ebbde2f4c315a33f
Siddharth Srivastava <sidmkp96@gmail.com>
2014-01-26 02:29:43 +0530

    Created common classes to read file data

---
+++ WindowsTweaker/FileItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace WindowsTweaker {
+    class FileItem {
+        public FileItem(string name, ImageSource iconAssociated) {
+            this.name = name;
+            this.iconAssociated = iconAssociated;
+        }
+
+        private String name;
+        private ImageSource iconAssociated;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ImageSource IconAssociated {
+            get { return iconAssociated; }
+            set { iconAssociated = value; }
+        }
+    }
+}
