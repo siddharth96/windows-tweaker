54de9e1d616035eb2a69747c100f36ca7b4a59ed
Siddharth Srivastava <sidmkp96@gmail.com>
2014-04-13 23:26:44 +0530

    Culture specific res dicts & string shufflings
    Moved all the strings till Logon Tab to StringResources for english
    language, and created empty resource dicts for other languages

--- WindowsTweaker/App.xaml.cs
+++ WindowsTweaker/App.xaml.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
-namespace WindowsTweaker
-{
+namespace WindowsTweaker {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
+
+        private void SetUiCulture(string cultureName = "en-US") {
+            CultureInfo ci = new CultureInfo(cultureName);
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        private void SetLanguageDictionary() {
+            List<ResourceDictionary> resourceDictionaries = new List<ResourceDictionary>();
+            foreach (ResourceDictionary mergedDictionary in
+                this.Resources.MergedDictionaries) {
+                resourceDictionaries.Add(mergedDictionary);
+            }
+            string culture = Thread.CurrentThread.CurrentCulture.ToString();
+            string lang = culture.Contains("-") ? culture.Split('-')[0] : culture;
+            string requestedCulture = String.Format("Resources/StringResources.{0}.xaml", lang);
+            ResourceDictionary resourceDictionary = resourceDictionaries.FirstOrDefault(x => x.Source.OriginalString == requestedCulture);
+            if (resourceDictionary == null) {
+                requestedCulture = "Resources/StringResources.xaml";
+                resourceDictionary = resourceDictionaries.FirstOrDefault(x => x.Source.OriginalString == requestedCulture);
+            }
+            if (resourceDictionary != null) {
+                this.Resources.MergedDictionaries.Remove(resourceDictionary);
+                this.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+        }
+
+        private void OnApplicationStartup(object sender, StartupEventArgs e) {
+            SetUiCulture("ru-RU");
+            SetLanguageDictionary();
+        }
     }
 }
